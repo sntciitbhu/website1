3c3999165a7a62351bd9cbb2dc7be214e23e5364
Subodh <vermasubodhk@gmail.com>
2019-10-22 23:36:28 +0530

    Debug=True as static files were not working with Debug=False and collected static

--- sntc/settings.py
+++ sntc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kjiabn!mx(oawo=e%#@i!=#t9%x^_zu-(lv$u4(e53dcd&)$^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
